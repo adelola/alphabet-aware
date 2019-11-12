296251da0c42e7eb6481d428b63a4aaee8a30352
adelola <lola.adekunle@gmail.com>
2019-11-12 16:23:31 -0600

    Configure webpack

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,8 +1,19 @@
 class Challenge < ApplicationRecord
   belongs_to :student
 
+  validates :type, presence: true
+  validates :date, presence: true
+
   has_many :correct_answers
   has_many :incorrect_answers
 
+  def all_lowercase_letters
+    @lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  end
+
+  def all_capital_letters
+    @capital = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  end
+  
   
 end
